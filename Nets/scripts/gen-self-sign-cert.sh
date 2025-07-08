5df5e93a270eb85930a38210ec9369c0d304f49b
ibra-kdbra <git-repo-develop@outlook.com>
2025-07-08 13:17:08 +0300

    build, debug, gen-self-sign, generate, install, update

---
+++ Nets/scripts/gen-self-sign-cert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! openssl version ; then
+    echo "Failed to get OpenSSL version, please check if it's installed."
+    exit
+fi
+
+echo ""
+echo "Generating self signed certificate..."
+
+openssl req -x509 -nodes -newkey rsa:2048 -sha256 -keyout private-key.pem -out certificate.pem -days 365 -subj "/CN=localhost"
